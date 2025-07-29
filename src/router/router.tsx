559a454238dfdd552dfebbdb0db6411fa76e4d9b
Longle290204 <longvis2004@gmail.com>
2025-07-29 10:27:45 +0700

    feat(router): add route

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,7 +1,14 @@
 import { RouteItem } from '../types/interfaces/router.js'
 
 import LoginPage from '../pages/auth/login/LoginPage.js'
+import DashboardLayout from '~/layouts/main/dashboard-layout/index.js'
+import Dashboard from '~/pages/dashboard/index.js'
+import Group from '~/pages/group/index.js'
 
-const publicRoutes: RouteItem[] = [{ path: '/login', element: LoginPage }]
+const publicRoutes: RouteItem[] = [
+  { path: '/login', element: LoginPage },
+  { path: '/dashboard', element: Dashboard, layout: DashboardLayout },
+  { path: '/group', element: Group, layout: DashboardLayout }
+]
 
 export { publicRoutes }
